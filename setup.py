9bb580947cac850cbccf7088ef5ef022221ef706
agnagnagnagn <annotator-agn@outlook.com>
2024-10-14 21:25:20 +0800

    ver Oct14th
    updated package name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup( name="my-python-package"
+setup( name="my-python-package-agn"
      , version="0.1"
      , install_requires=["numpy"]
      , packages=find_packages()
